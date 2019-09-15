ee957f4f71dafd9e91b263906e78206932372b0a
smbeaupre <55219660+smbeaupre@users.noreply.github.com>
2019-09-15 18:26:56 -0400

    Set time constraint for minimax

--- src/agents/minimax.py
+++ src/agents/minimax.py
@@ -1,15 +1,17 @@
 import random
+import time
 from copy import deepcopy
 from src.agents.evaluators import boolean_eval
 
 
 class MinimaxPlayer:
 
-    def __init__(self, token, max_depth=5, move_selection='Valid'):
+    def __init__(self, token, max_depth=50, search_time=15, move_selection='Valid'):
         self.token = token
         self.max_depth = max_depth
         self.move_selection = move_selection
         self.number_searches = 0
+        self.search_time = search_time
         if self.token == 'O':
             self.min_token = 'X'
         else:
@@ -17,20 +19,20 @@ class MinimaxPlayer:
 
     def get_move(self, game):
 
-        score, move = self._minimax(current_game=game, current_depth=0, is_max_turn=True)
+        score, move = self._minimax(current_game=game, current_depth=0, is_max_turn=True, max_time=time.time() + self.search_time)
 
         if move == "":
             return random.choice(game.get_valid_moves())
         return move
 
-    def _minimax(self, current_game, current_depth, is_max_turn, evaluator=boolean_eval, alpha=float('-inf'), beta=float('inf')):
+    def _minimax(self, current_game, current_depth, is_max_turn, max_time, evaluator=boolean_eval, alpha=float('-inf'), beta=float('inf')):
 
         if is_max_turn:
             current_token = self.token
         else:
             current_token = self.min_token
 
-        if current_game.check_winner() != ' ' or current_depth == self.max_depth:
+        if current_game.check_winner() != ' ' or current_depth == self.max_depth or time.time() >= max_time:
             return evaluator(current_game, self.token), ""
 
         if self.move_selection == 'Likely':
@@ -46,7 +48,7 @@ class MinimaxPlayer:
 
             new_game = deepcopy(current_game).perform_move(move, current_token)
 
-            eval_child, action_child = self._minimax(current_game=new_game, current_depth=current_depth + 1, is_max_turn=not is_max_turn, alpha=alpha, beta=beta)
+            eval_child, action_child = self._minimax(current_game=new_game, current_depth=current_depth + 1, max_time=max_time, is_max_turn=not is_max_turn, alpha=alpha, beta=beta)
 
             if is_max_turn and best_value < eval_child:
                 best_value = eval_child
